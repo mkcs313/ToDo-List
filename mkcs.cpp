3bca08872fef5f6b7337be6651a59a699aa4ab5b
mkcs313 <mkcs313@gmail.com>
2020-11-13 22:44:49 +0530

    my first commit

---
+++ mkcs.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+   // ifstream("input.txt");
+   // ofstream("output.txt");
+
+   // string s;   cin >> s;
+    
+    cout << "Hello" << "\n";
+    
+    return 0;
+    
+}
